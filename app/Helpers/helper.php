b0a2f2785ca3710d4e71ff351255871510fa960d
zhaoyang <602155776@qq.com>
2019-06-26 16:09:15 +0800

    首页基础页面

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -1,2 +1,8 @@
 <?php
-
+/**
+ * @return mixed
+ * 用于根据路由更换css class
+ */
+function route_class() {
+    return str_replace('.', '-', Route::currentRouteName());
+}
---
+++ resources/views/layouts/_footer.blade.php
@@ -0,0 +1,10 @@
+<footer class="footer">
+    <div class="container">
+        <p class="pull-left">
+            由<a href="#">wo</a>编写
+        </p>
+        <p class="pull-right">
+            <a href="#">联系我们</a>
+        </p>
+    </div>
+</footer>
