9079bb13795404b916d725462ce68b9140c84c55
zhaoyang <602155776@qq.com>
2019-06-28 15:38:20 +0800

    新增用户帖子创建功能

--- app/Helpers/helper.php
+++ app/Helpers/helper.php
@@ -6,3 +6,15 @@
 function route_class() {
     return str_replace('.', '-', Route::currentRouteName());
 }
+
+/**
+ * @param $value
+ * @param int $length
+ * @return string
+ *
+ * 摘录的方法实现
+ */
+function make_excerpt($value, $length=200) {
+    $excerpt = trim(preg_replace('/\r\n|\r|\n+/', ' ', strip_tags($value)));
+    return str_limit($excerpt, $length);
+}
