f293b1d6158509f48464c1f95c65d264b05c9456
zhaoyang <602155776@qq.com>
2019-06-28 18:02:09 +0800

    xxs 漏洞解决

--- resources/views/topics/create_and_edit.blade.php
+++ resources/views/topics/create_and_edit.blade.php
@@ -5,26 +5,26 @@
 @endsection
 
 @section('scripts')
-    <script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>
-    <script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>
+    {{--<script type="text/javascript"  src="{{ asset('js/module.js') }}"></script>--}}
+    {{--<script type="text/javascript"  src="{{ asset('js/hotkeys.js') }}"></script>--}}
+    {{--<script type="text/javascript"  src="{{ asset('js/uploader.js') }}"></script>--}}
+    {{--<script type="text/javascript"  src="{{ asset('js/simditor.js') }}"></script>--}}
 
-    <script>
-        $(document).ready(function () {
-            let editor = new Simditor({
-                textarea: $('#editor'),
-                upload: {
-                    url: '{{ route('topics.upload_image') }}',
-                    params: { _token : '{{ csrf_token() }}'},
-                    fileKey: 'upload_file',
-                    connectionCount: 3,
-                    leaveConfirm: '文件上传中。。。'
-                },
-                pasteImage: true
-            })
-        })
-    </script>
+    {{--<script>--}}
+        {{--$(document).ready(function () {--}}
+            {{--let editor = new Simditor({--}}
+                {{--textarea: $('#editor'),--}}
+                {{--upload: {--}}
+                    {{--url: '{{ route('topics.upload_image') }}',--}}
+                    {{--params: { _token : '{{ csrf_token() }}'},--}}
+                    {{--fileKey: 'upload_file',--}}
+                    {{--connectionCount: 3,--}}
+                    {{--leaveConfirm: '文件上传中。。。'--}}
+                {{--},--}}
+                {{--pasteImage: true--}}
+            {{--})--}}
+        {{--})--}}
+    {{--</script>--}}
 @endsection
 @section('content')
     <div class="container">
