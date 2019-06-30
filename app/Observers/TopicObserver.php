3f547fd52e190829993f320a64a1ede56da65c37
zhaoyang <602155776@qq.com>
2019-06-30 19:20:35 +0800

    seo url

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use App\Handlers\SlugTranslateHandler;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -21,7 +22,15 @@ class TopicObserver
 
     public function saving(Topic $topic)
     {
+        //xss 过滤
         $topic->body = clean($topic->body, 'user_topic_body');
+
+        //生成摘录
         $topic->excerpt = make_excerpt($topic->body);
+
+        //slug
+        if (! $topic->slug) {
+            $topic->slug = app(SlugTranslateHandler::class)->translate($topic->title);
+        }
     }
 }
